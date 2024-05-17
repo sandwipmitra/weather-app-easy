02f823edb4989e50e53b6beec28e3d68bb107ecc
sand <sandmitra@outlook.com>
2024-05-17 08:47:31 +0800

    initial-commit-3

--- script.js
+++ script.js
@@ -1,21 +1,41 @@
 const apiKey = "473afae85b3295dd5b7137c0af8c6ed5";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=Khulna";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
-async function checkWeather() {
-    const response = await fetch(apiUrl + `&appid=${apiKey}`);
+const searchBox = document.querySelector(".search input")
+const searchBtn = document.querySelector(".search button")
+const weatherIcon = document.querySelector(".weather-icon")
+
+
+async function checkWeather(city) {
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
 
     let data = await response.json();
 
     console.log(data);
 
     document.querySelector(".city").innerHTML = data.name;
-    document.querySelector(".temp").innerHTML = data.main.temp + "°C";
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
     document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
     document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
 
-
-
+    if (data.weather[0].main == "Clear") {
+        weatherIcon.src = "images/clear.png"
+    } else if (data.weather[0].main == "Clouds") {
+        weatherIcon.src = "images/clouds.png"
+    } else if (data.weather[0].main == "Drizzle") {
+        weatherIcon.src = "images/drizzle.png"
+    } else if (data.weather[0].main == "Humidity") {
+        weatherIcon.src = "images/humidity.png"
+    } else if (data.weather[0].main == "Mist") {
+        weatherIcon.src = "images/mist.png"
+    } else if (data.weather[0].main == "Rain") {
+        weatherIcon.src = "images/rain.png"
+    }
 
 }
-checkWeather();
+
+searchBtn.addEventListener("click", () => {
+    checkWeather(searchBox.value);
+})
+
 
